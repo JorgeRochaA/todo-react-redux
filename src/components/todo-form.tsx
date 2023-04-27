2ad443ccf29c12998c56a66241a855b53c510c52
JorgeRochaA <jor2019.cr@gmail.com>
2023-04-27 15:16:33 -0600

    refactor : some changes

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -1,17 +1,12 @@
 import "../styles/todo-form.css";
 
 export default function TodoForm() {
-  
   return (
     <>
       <h2>Add New Task Form</h2>
       <form>
         <input type="text" placeholder="Task Title" />
         <input type="text" placeholder="Task Description" />
-        <div className="checkbox-container">
-          <input type="checkbox" />
-          <label htmlFor="task name">Completed</label>
-        </div>
         <button>Add Task</button>
       </form>
     </>
