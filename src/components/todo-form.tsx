b25ed23690c95f3014fc0b348fd67e892a61cb71
JorgeRochaA <jor2019.cr@gmail.com>
2023-05-03 11:05:23 -0600

    feat : add react-hook-form validation

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -1,50 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { sendFormData } from "../redux/features/form/formSlice";
 import type { RootState } from "../redux/store";
 import "../styles/todo-form.css";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  title: string;
+  description: string;
+};
 
 export default function TodoForm() {
   const { status, error } = useSelector((state: RootState) => state.todoForm);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
   const dispatch = useDispatch();
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-  });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(sendFormData(formData));
+  const onSubmit = (data: any) => {
+    dispatch(sendFormData(data));
   };
 
   useEffect(() => {
     if (status === "succeeded") {
-      setFormData({ title: "", description: "" });
+      reset();
     }
   }, [status]);
 
   return (
     <>
       <h2>Add New Task Form</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          name="title"
           type="text"
           placeholder="Task Title"
-          value={formData.title}
-          onChange={handleChange}
+          {...register("title", { required: true })}
         />
+        {errors.title && (
+          <span className="error-message">This field is required.</span>
+        )}
         <textarea
-          name="description"
           placeholder="Task Description"
-          value={formData.description}
-          onChange={handleChange}
+          {...register("description", { required: true })}
         />
+        {errors.description && (
+          <span className="error-message">This field is required.</span>
+        )}
         <button>Add Task</button>
       </form>
     </>
